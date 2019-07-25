369e9610d588af3a584719d4c2186c79003f5982
Michael Schwartz <mykesx@gmail.com>
2019-07-24 18:15:41 -0700

    fix motion detector always changing the level

--- rules/MotionLights.js
+++ rules/MotionLights.js
@@ -13,18 +13,19 @@ class MotionLights extends Rule {
     this.sensor.on("statechange", newState => {
       log("MOTION statechange", newState);
       if (newState.motion === "active") {
-        console.log("motion", this.light.state);
+        log("motion", this.light.state);
         const state = this.light.state;
         const d = new Date(),
           h = d.getHours();
         if (state.switch === "on") {
-          if (h >= 21 || h < 6) {
-            log("detected motion, overnight level 5");
-            this.assure(this.light.name, "level", 5, false);
-          } else {
-            log("detected motion, daytime level 100");
-            this.assure(this.light.name, "level", 100, false);
-          }
+          // leave it on
+          //          if (h >= 21 || h < 6) {
+          //            log("detected motion, overnight level 5");
+          //            this.assure(this.light.name, "level", 5, false);
+          //          } else {
+          //            log("detected motion, daytime level 100");
+          //            this.assure(this.light.name, "level", 100, false);
+          //          }
         } else {
           this.assure(this.light.name, "switch", "on", false);
           this.light.once("statechange", newState => {
