18a875db72ca918d8eadadd3c3c67126080f9587
Michael Schwartz <mykesx@gmail.com>
2020-12-03 14:35:09 -0800

    exits on RESTART message

--- index.js
+++ index.js
@@ -2,13 +2,49 @@ process.env.DEBUG += ":OUTDOORS";
 process.title = process.env.TITLE || "triggers-microservice";
 
 const schedule = require("./lib/Schedule"),
+  MQTT = require("mqtt"),
+  os = require("os"),
   weather = require("./lib/Weather"),
   console = require("console");
 
 require("./lib/Things");
 require("./lib/Rules");
 
+function exit(client, message) {
+  try {
+    const packet = JSON.stringify({
+      type: "alert",
+      host: os.hostname(),
+      title: "WARNING",
+      message: [message],
+    });
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("alert", message);
+    client.publish("alert", packet, { retain: false });
+  } catch (e) {}
+  setTimeout(() => {
+    process.exit(0);
+  }, 3000);
+}
+
 function main() {
+  const client = MQTT.connect(process.env.MQTT_HOST);
+  client.on("connect", () => {
+    const packet = JSON.stringify({
+      type: "alert",
+      host: os.hostname(),
+      title: "WARNING",
+      message: [`${process.title} running`],
+    });
+    client.publish("alert", packet, { retain: false });
+    client.subscribe("triggers/reset/#");
+  });
+  client.on("message", () => {
+    exit(client, "triggers-microservice restarting");
+  });
   schedule.on("each-minute", (date) => {
     try {
       console.log(
