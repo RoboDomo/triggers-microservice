6af3a774425d69637ce24202e79efa7a17000f07
Michael Schwartz <mykesx@gmail.com>
2021-07-11 16:07:11 -0700

    seems to be workin

--- rules/Atmos.js
+++ rules/Atmos.js
@@ -9,35 +9,49 @@ const Rule = require("../lib/Rule"),
   things = require("../lib/Things"),
   log = require("../lib/log");
 
+const ATMOS = "DOLBY ATMOS";
+
 class Atmos extends Rule {
   constructor() {
     super();
-    this.mv = null;
-    this.defaultVolume = "30";
+    this.mv = "30";
+    this.ms = null;
 
     try {
       //      console.log("things", Object.keys(things));
       const thing = things["Theater AVR"];
       const vol_topic = thing.topic + "/set/MV";
       thing.on("statechange", (newState, oldState) => {
-        if (newState.MV && newState.MV !== oldState.MV || newState.MS !== oldState.MS) {
-          if (this.mv !== newState.MV) {
-//            console.log("new volume", this.mv, newState.MV);
+        if (newState.MS && oldState.MS) {
+          if (
+            newState.MV != this.mv &&
+            (
+              this.ms != ATMOS &&
+              newState.MS.toUpperCase() !== ATMOS &&
+              oldState.MS.toUpperCase() !== ATMOS
+            )
+          ) {
             this.mv = newState.MV;
-//            console.log("new volume", this.mv, newState.MV);
+            console.log(
+              "new volume",
+              this.mv,
+              this.ms,
+              newState.MS,
+              oldState.MS,
+            );
           }
-          this.mv = newState.mv;
-          if (newState.MS != oldState.MS) {
-            const newVol = (newState.MS.toUpperCase() === "DOLBY ATMOS")
-              ? "MV60"
-              : `MV${this.defaultVolume}`;
-
-            this.publish(vol_topic+newVol.toString(), newVol);
-
-//            console.log("newState", newState.MS, newVol);
-          } else if (newState.MS && newState.MS.toUpperCase() !== "DOLBY ATMOS" && this.mv) {
-//            console.log("default volume", newState.MS, this.mv);
-            this.defaultVolume = this.mv;
+          if (newState.MS != this.ms) {
+            this.ms = newState.MS;
+            console.log("statechange", this.ms, newState.MS, oldState.MS);
+            if (newState.MS.toUpperCase() === ATMOS) {
+              console.log("new state ATMOS");
+              this.publish(vol_topic + "MV60", "MV60");
+            } else {
+              console.log("new state NOT ATMOS", oldState.MS.toUpperCase());
+              if (oldState.MS.toUpperCase() === ATMOS) {
+                this.publish(vol_topic + "MV" + this.mv, "MV" + this.mv);
+              }
+            }
           }
         }
 
@@ -46,7 +60,7 @@ class Atmos extends Rule {
     } catch (e) {
       console.log("E", e);
     }
-//    console.log("x");
+    //    console.log("x");
   }
 }
 
